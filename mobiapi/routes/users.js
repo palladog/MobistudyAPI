d74be3538d217aa320b920e90840abdaf3d9a171
Unknown <arvingo@yahooo.co.uk>
2018-10-17 10:04:00 +0100

    Added Getall and Post Methods

--- mobiapi/routes/users.js
+++ mobiapi/routes/users.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
+var Database = require('arangojs');
+var db = new Database({ url:'http://127.0.0.1:8529'});
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+db.useDatabase('nodeArangoWebAppDB');
+db.useBasicAuth('root','');
+
+const taskCollection = db.collection('User');
+
+router.get('/getAllStudies', function(req, res){
+  taskCollection
+  .all()
+  .then(function(response) {
+    console.log(`Retrieved documents.`, response._result);
+    return res.status(200).json(response._result);
+  })
+  .catch(function(error) {
+    console.error('Error getting document', error);
+    return res.status(500).json(error);
+  });
 });
-
+router.post('/postNewStudy', function (req, res) {
+  console.log("↓↓↓↓ Add New Study ↓↓↓↓");
+  var study = {
+    "studyjson": req.body.userstudy,
+    // _key: 'firstDocument',
+    c: Date()
+  };
+  taskCollection
+  .all()
+  .then(function(response) {
+  return taskCollection.save(study);
+  })
+  .catch(function(error) {
+    console.error('Error getting document', error);
+    return res.status(500).json(error);
+  });
+});
 module.exports = router;
