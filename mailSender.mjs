e0427352894d177f1b02f0068aae9bed7494b229
Dario Salvi <dario.salvi.work@gmail.com>
2019-02-11 11:19:39 +0000

    added logging on email sender

--- mailSender.mjs
+++ mailSender.mjs
@@ -1,6 +1,7 @@
 import googleAPIs from 'googleapis'
 import nodemailer from 'nodemailer'
 import getConfig from './config.mjs'
+import { applogger } from './logger'
 
 const config = getConfig()
 
@@ -11,33 +12,37 @@ export async function sendEmail (contact, subject, message) {
     'https://developers.google.com/oauthplayground'
   )
   return new Promise(async (resolve, reject) => {
-    oAuth2Client.setCredentials({
-      refresh_token: config.gmail.refresh_token
-    })
-    let res = await oAuth2Client.refreshAccessToken()
-    const smtpTransport = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        type: 'OAuth2',
-        user: config.gmail.email,
-        clientId: config.gmail.client_id,
-        clientSecret: config.gmail.client_secret,
-        refreshToken: config.gmail.refresh_token,
-        accessToken: res.credentials.access_token
+    try {
+      oAuth2Client.setCredentials({
+        refresh_token: config.gmail.refresh_token
+      })
+      let res = await oAuth2Client.refreshAccessToken()
+      const smtpTransport = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          type: 'OAuth2',
+          user: config.gmail.email,
+          clientId: config.gmail.client_id,
+          clientSecret: config.gmail.client_secret,
+          refreshToken: config.gmail.refresh_token,
+          accessToken: res.credentials.access_token
+        }
+      })
+
+      const mailOptions = {
+        from: config.gmail.email,
+        to: contact,
+        subject: subject,
+        generateTextFromHTML: true,
+        html: message
       }
-    })
 
-    const mailOptions = {
-      from: config.gmail.email,
-      to: contact,
-      subject: subject,
-      generateTextFromHTML: true,
-      html: message
+      smtpTransport.sendMail(mailOptions, (error, response) => {
+        error ? reject(error) : resolve(response)
+        smtpTransport.close()
+      })
+    } catch (error) {
+      applogger.error(error, 'Cannot send email to ' + contact)
     }
-
-    smtpTransport.sendMail(mailOptions, (error, response) => {
-      error ? reject(error) : resolve(response)
-      smtpTransport.close()
-    })
   })
 }
